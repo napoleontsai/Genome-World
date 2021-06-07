80cb87839d9228423d5cc0667d45a7be79b47437
napoleontsai <48658540+napoleontsai@users.noreply.github.com>
2021-06-07 13:13:42 -0400

    Add files via upload

---
+++ CID_cy_ions.R
@@ -0,0 +1,60 @@
+
+# Calculate all possible CID fragments
+library(stringr)
+library(seqinr)
+rnase.digested.fragments <- read.csv(as.character(read.table("my path/FileDirect.txt")$V1[2]),header = TRUE) # FileDirect.txt file path
+cid.frag.array <- list()
+for (i in 1:nrow(rnase.digested.fragments)){
+  cid.frag <- list()
+  for (j in 1:str_length(rnase.digested.fragments$Digested_Sequence[i])){
+    cid.frag[[j]] <- substring(as.character(rnase.digested.fragments$Digested_Sequence[i]),j,c(j:str_length(rnase.digested.fragments$Digested_Sequence[i])))
+  }
+  cid.frag.array[[i]] <- cid.frag
+}
+cid.frag.array <- sapply(cid.frag.array,unlist)
+
+# Select c-y ions
+c.ions <- list()
+for (i in 1:length(cid.frag.array)){
+  c.ions[[i]] <- cid.frag.array[[i]][1:max(str_count(cid.frag.array[[i]]))-1]
+}
+y.ions <- list()
+for (i in 1:nrow(rnase.digested.fragments)){
+  y.ions[[i]] <- substring(rnase.digested.fragments$Digested_Sequence[i],c(2:str_count(rnase.digested.fragments$Digested_Sequence[i])))
+}
+c.ions <- unlist(c.ions[which(str_count(rnase.digested.fragments$Digested_Sequence) > 1)])
+y.ions <- unlist(y.ions[which(str_count(rnase.digested.fragments$Digested_Sequence) > 1)])
+
+# Calculate c-y ion fragments mass
+mass_A <- list()
+mass_G <- list()
+mass_C <- list()
+mass_U <- list()
+c_ions_mass <- list()
+for (i in 1:length(c.ions)){
+  mass_A[[i]] <- str_count(c.ions[[i]],"A")*329.2
+  mass_G[[i]] <- str_count(c.ions[[i]],"G")*345.2
+  mass_C[[i]] <- str_count(c.ions[[i]],"C")*305.2
+  mass_U[[i]] <- str_count(c.ions[[i]],"T")*306.2
+  c_ions_mass[[i]] <- mass_A[[i]] + mass_C[[i]] + mass_G[[i]] + mass_U[[i]] + 1
+}
+c.ions.frag.mass <- cbind(c.ions,unlist(c_ions_mass))
+
+mass_A <- list()
+mass_G <- list()
+mass_C <- list()
+mass_U <- list()
+y_ions_mass <- list()
+for (i in 1:length(c.ions)){
+  mass_A[[i]] <- str_count(y.ions[[i]],"A")*329.2
+  mass_G[[i]] <- str_count(y.ions[[i]],"G")*345.2
+  mass_C[[i]] <- str_count(y.ions[[i]],"C")*305.2
+  mass_U[[i]] <- str_count(y.ions[[i]],"T")*306.2
+  y_ions_mass[[i]] <- mass_A[[i]] + mass_C[[i]] + mass_G[[i]] + mass_U[[i]] + 17
+}
+y.ions.frag.mass <- cbind(y.ions,unlist(y_ions_mass))
+colnames(c.ions.frag.mass) <- c("c_fragments","Mass_Da")
+colnames(y.ions.frag.mass) <- c("y_fragments","Mass_Da")
+write.csv(c.ions.frag.mass, 'output folder path/std_out_c_ions.csv') # Output folder path
+write.csv(y.ions.frag.mass, 'output folder path/std_out_y_ions.csv') # Output folder path
+q()
---
+++ RNaseA.R
@@ -0,0 +1,53 @@
+
+# Calculate post enzyme-digested oligo sequences
+library(stringr)
+library(seqinr)
+FastaFile <- read.fasta(as.character(read.table("my path/FileDirect.txt")$V1[1])) # FindDirect.txt file path
+frag.one <- unlist(strsplit(str_flatten(FastaFile[[1]]),"c"))
+digest.frag.one <- str_remove(paste(frag.one,"c")," ")
+t_frag <- strsplit(digest.frag.one[grep("t",digest.frag.one)],"t")
+t_frag_laststr <- list()
+for(i in 1:length(t_frag)){
+  t_frag_laststr[[i]] <- t_frag[[i]][length(t_frag[[i]])]
+}
+t_frag_laststr_rm <- list()
+for(i in 1:length(t_frag)){
+  t_frag_laststr_rm[[i]] <- t_frag[[i]][1:(length(t_frag[[i]])-1)]
+}
+t_paste <- list()
+for(i in 1:length(t_frag_laststr_rm)){
+  t_paste[[i]] <- str_remove(paste(t_frag_laststr_rm[[i]],"t")," ")
+}
+digest.frag.two <- list()
+for(i in 1:length(t_paste)){
+  digest.frag.two[[i]] <- unlist(c(t_paste[[i]],t_frag_laststr[[i]]))
+}
+digest.frag.one[grep("t",digest.frag.one)] = c(digest.frag.two)
+digest.frag.two.df <- unlist(digest.frag.one)
+if (FastaFile[[1]][length(FastaFile[[1]])] == "c"){
+  digest.frag.two.df[length(digest.frag.two.df)] <- digest.frag.two.df[length(digest.frag.two.df)]
+} else{
+  digest.frag.two.df[length(digest.frag.two.df)] <- str_remove(digest.frag.two.df[length(digest.frag.two.df)],"c")
+}
+if(digest.frag.two.df[length(digest.frag.two.df)] == ""){
+  digest.frag.two.df <- digest.frag.two.df[1:length(digest.frag.two.df)-1]
+} else {
+  digest.frag.two.df <- digest.frag.two.df[1:length(digest.frag.two.df)]
+}
+digest.frag.two.df <- as.data.frame(digest.frag.two.df)
+RNaseA_digested_frag <- na.omit(apply(digest.frag.two.df,2,str_to_upper))
+
+# Calculate mass of the digested fragments
+temp_frag_split <- strsplit(RNaseA_digested_frag[1:length(RNaseA_digested_frag)],"")
+mass_A <- str_count(temp_frag_split,"A")*329.2
+mass_G <- str_count(temp_frag_split,"G")*345.2
+mass_C <- str_count(temp_frag_split,"C")*305.2
+mass_U <- str_count(temp_frag_split,"T")*306.2
+oligo_mass <- list()
+for(i in 1:length(temp_frag_split)){
+  oligo_mass[i] <- mass_A[i] + mass_C[i] + mass_G[i] + mass_U[i] + 18
+}
+RNaseA_digested_frag <- cbind(RNaseA_digested_frag, unlist(oligo_mass))
+colnames(RNaseA_digested_frag) <- c("Digested_Sequence","Oligo_Mass_Da")
+write.csv(RNaseA_digested_frag,'output folder path/std_out_RNaseA.csv') # Output folder path
+q()
